d7f6e651ab1f644412b7e554fbb3c19efd97746b
Matt Mattera <mattmatts0819@gmail.com>
2026-02-26 17:24:53 -0500

    functionality for personal and master schedules

--- src/edu/psu/ist/Controllers/MasterScheduleAPIController.java
+++ src/edu/psu/ist/Controllers/MasterScheduleAPIController.java
@@ -3,7 +3,7 @@ import edu.psu.ist.Models.MasterScheduleModel;
 
 public class MasterScheduleAPIController {
 
-    //MASTER SCHEDULE
+    //MASTER SCHEDUL
     // get master schedule (all events) for a specific festival
     public MasterScheduleModel getMasterSchedule(int festivalId) {
         System.out.println("Getting master schedule for festival id: " + festivalId);
